3829ea4876ca874d5baafc79807b762f7ced280e
Jay Flowers <jflowers@microstrategy.com>
2016-08-15 14:12:00 -0400

    cleanup

--- workspace-settings/rake/lib/tasks/vagrant/virtualbox/trivia/tasks.rb
+++
@@ -1,6 +0,0 @@
-require 'vagrant/rake/provider/virtualbox'
-
-provider = Vagrant::Rake::Provider::VirtualBox.new
-provider.generate_tasks
-
-task :"deploy_#{$WORKSPACE_SETTINGS[:project_name]}" => [:move_artifact]
--- workspace-settings/ruby/example-master/lib/example.rb
+++
@@ -1,7 +0,0 @@
-module SlackGem
-  class Example
-    def self.gem_name
-      'example-master'
-    end
-  end
-end
