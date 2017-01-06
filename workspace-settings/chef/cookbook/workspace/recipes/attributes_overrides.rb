4f522b2d8ba0e627fe9af5351d60ea3c6e2757ae
Wu Chenhao <chwu@microstrategy.com>
2017-01-06 08:35:20 -0500

    create erb for workspace cookbook

--- workspace-settings/chef/cookbook/workspace/recipes/attributes_overrides.rb
+++
@@ -1,6 +0,0 @@
-#
-# Cookbook Name:: workspace
-# Recipe:: attributes_overrides
-#
-
-node.override[:virtualbox][:version]  = '5.0.20-106931'
--- workspace-settings/chef/cookbook/workspace/recipes/project.rb
+++
@@ -1,8 +0,0 @@
-#
-# Cookbook Name:: workspace
-# Recipe:: project
-#
-
-include_recipe 'chef_commons'
-include_recipe 'workspace::attributes_overrides'
-
