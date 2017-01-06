c087a8787b565f55913c73cfdd8ad50608a02e45
Wu Chenhao <chwu@microstrategy.com>
2017-01-06 13:02:59 -0500

    update rake tasks

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -8,18 +8,7 @@ task :build do
   good "Puts your build command here, for example mvn compile or grable build"
 end
 
-desc "test project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
-task :test do
-  good "Puts your test command here, for example mvn test or grable test"
-end
-
-
 desc "package project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 task :package do
   good "Puts your pacakge command here."
-end
-
-desc "deploy project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
-task :deploy, :version do |t, args|
-  good "Puts deployment code here."
 end
--- workspace-settings/rake/lib/tasks/hat/pipeline.rb
+++ workspace-settings/rake/lib/tasks/hat/pipeline.rb
@@ -1,3 +1,8 @@
 
 task :prepush_test => [:rt_deploy]
 task :upload => [:prepush_test]
+
+desc "deploy project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
+task :deploy, :version do |t, args|
+  good "Puts deployment code here."
+end
