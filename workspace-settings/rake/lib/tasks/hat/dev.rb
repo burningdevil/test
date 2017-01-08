cddf0dff7f7aa520fdd7377018ae777e524fb5d8
Wu Chenhao <chwu@microstrategy.com>
2017-01-07 21:06:26 -0500

    add file localtion and line number

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -12,7 +12,5 @@ end
 
 desc "package project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 task :package do
-  caller_infos = caller.first.split(":")
-  good "#{caller_infos[0]} : line #{caller_infos[1]}"
-  good "Puts your pacakge command here."
+  good "Puts your pacakge command here: #{__FILE__}:#{__LINE__}"
 end
--- workspace-settings/rake/lib/tasks/hat/pipeline.rb
+++ workspace-settings/rake/lib/tasks/hat/pipeline.rb
@@ -4,5 +4,5 @@ desc "deploy project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:ho
 task :deploy, :version do |t, args|
   caller_infos = caller.first.split(":")
   good "#{caller_infos[0]} : line #{caller_infos[1]}"
-  good "Puts deployment code here."
+  good "Puts deployment code here: #{__FILE__}:#{__LINE__}"
 end
