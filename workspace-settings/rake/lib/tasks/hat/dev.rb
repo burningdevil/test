fedc0ed8ce8a78d028b527013bb09f61d2d2b657
Wu Chenhao <chwu@microstrategy.com>
2017-01-07 20:32:34 -0500

    add file localtion and line number

--- workspace-settings/rake/lib/tasks/hat/dev.rb
+++ workspace-settings/rake/lib/tasks/hat/dev.rb
@@ -5,10 +5,14 @@ include ShellHelper::Shell
 
 desc "build project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 task :build do
+  caller_infos = caller.first.split(":")
+  good "#{caller_infos[0]} : line #{caller_infos[1]}"
   good "Puts your build command here, for example mvn compile or grable build"
 end
 
 desc "package project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 task :package do
+  caller_infos = caller.first.split(":")
+  good "#{caller_infos[0]} : line #{caller_infos[1]}"
   good "Puts your pacakge command here."
 end
--- workspace-settings/rake/lib/tasks/hat/pipeline.rb
+++ workspace-settings/rake/lib/tasks/hat/pipeline.rb
@@ -2,5 +2,7 @@ Rake::Task[:upload].clear_prerequisites
 
 desc "deploy project in #{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}"
 task :deploy, :version do |t, args|
+  caller_infos = caller.first.split(":")
+  good "#{caller_infos[0]} : line #{caller_infos[1]}"
   good "Puts deployment code here."
 end
