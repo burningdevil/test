37f5d7266f71b5ed05acb3f3e4fb75d4f315663a
Nick <nackerman@microstrategy.com>
2017-10-25 10:01:51 -0400

    pass artifact extension when creating pipeline rather than casuing failure

--- workspace-settings/rake/default.rb
+++ workspace-settings/rake/default.rb
@@ -5,6 +5,6 @@ raise "edit below with specifics for this project:
   replace the output location with where your artifact is built to
 "
 Compiler::Maven.artifact_info(
-  artifact_file_extension: "the file extension of the artifact: zip, jar, war, etc...",
-  output_dir: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}/TKN-.-project_name-.-TKN/.build/...",
+  artifact_file_extension: "TKN-.-artifact_file_extension-.-TKN",
+  output_dir: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}/TKN-.-project_name-.-TKN/.build",
 )
