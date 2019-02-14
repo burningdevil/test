718ff0928ae3c376c48c8ea650c609a680b02eba
Wu, Chenhao (Chuck) <chwu@microstrategy.com>
2019-02-14 17:22:08 -0500

    disable virtualbox deployment for dev and test workspace_setting

--- workspace-settings/shell/choices/test.bash
+++ workspace-settings/shell/choices/test.bash
@@ -3,9 +3,6 @@
 register_workspace_setting 'test'
 
 function set_workspace_settings_to_test() {
-  export VAGRANT_DEFAULT_PROVIDER=virtualbox
-  export VAGRANT_CONTEXT="${VAGRANT_DEFAULT_PROVIDER}/test"
- 
   export TEST_TYPES=acceptance
   export HATS=test
 }
