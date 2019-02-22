d2f32668c22364e1a4ec3520a3a9dd0c3a389b4e
Wu, Chenhao (Chuck) <chwu@microstrategy.com>
2019-02-22 14:59:34 -0500

    disable vagrant for fake

--- workspace-settings/shell/choices/fake.bash
+++ workspace-settings/shell/choices/fake.bash
@@ -3,8 +3,8 @@
 
 
 function set_workspace_settings_to_fake() {
-  export VAGRANT_DEFAULT_PROVIDER=virtualbox
-  export VAGRANT_CONTEXT="${VAGRANT_DEFAULT_PROVIDER}/fake"
+  # export VAGRANT_DEFAULT_PROVIDER=virtualbox
+  # export VAGRANT_CONTEXT="${VAGRANT_DEFAULT_PROVIDER}/fake"
   export TEST_TYPES=dev:acceptance
   export HATS=jenkins
 }
