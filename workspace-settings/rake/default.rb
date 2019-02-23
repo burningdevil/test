715e3bef55ca7b39540dd4b26f34bda1f402c625
Wu, Chenhao (Chuck) <chwu@microstrategy.com>
2019-02-23 00:32:38 -0500

    get rid of raise from default.rb file

--- workspace-settings/rake/default.rb
+++ workspace-settings/rake/default.rb
@@ -1,9 +1,5 @@
 require 'compiler/maven'
 
-raise "edit below with specifics for this project:
-  replace jar with your artifact version
-  replace the output location with where your artifact is built to
-"
 Compiler::Maven.artifact_info(
   artifact_file_extension: "TKN-.-artifact_file_extension-.-TKN",
   output_dir: "#{$WORKSPACE_SETTINGS[:paths][:project][:production][:home]}/TKN-.-project_name-.-TKN/.build",
