5805479f0348a64136f2d2b4e28d49fd03b253c9
Zhao, Sheng <szhao@microstrategy.com>
2019-06-07 17:11:47 -0400

    Add Types to web-components and use the first shared web-components

--- production/src/App.tsx
+++ production/src/App.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react'
+import { Button } from '@mstr/web-components'
+import '@mstr/web-components/dist/web-styles.css'
 import './app.scss'
-export default class App extends React.Component<{}, {}> {
+
+export default class App extends React.Component<any, any> {
   render() {
-    return <div className="">hello world!</div>
+    return (
+      <div>
+        <Button primary={true} text="Click me!"></Button>
+        ><div className="">hello world!</div>
+      </div>
+    )
   }
 }
