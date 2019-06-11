792e7ff4fd917c994e8908977329816231a2bd8d
Zhao, Sheng <szhao@microstrategy.com>
2019-06-11 16:44:32 -0400

    Add examples of multiple modules/pages

--- production/public/main.js
+++ production/public/main.js
@@ -1,7 +1,17 @@
 module.exports = {
-  openGeneric: function (info) {
-      return {
-          url: `/index.html?id=${info.objId}`
-      };
+  openModule1: function(info) {
+    return {
+      url: `/index.html#/m1?id=${info.objId}`
+    }
   },
-}; 
+  openModule2: function(info) {
+    return {
+      url: `/index.html#/m2?id=${info.objId}`
+    }
+  },
+  openModule3: function(info) {
+    return {
+      url: `/index.html#/m3?id=${info.objId}`
+    }
+  }
+}
