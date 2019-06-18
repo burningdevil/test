8b7daf2b802a4f487f82af0be5456a9a620a1e3b
Zhao, Sheng <szhao@microstrategy.com>
2019-06-18 15:50:09 -0400

    Add examples of using environment realted APIs.

--- production/public/main.js
+++ production/public/main.js
@@ -1,17 +1,22 @@
 module.exports = {
   openModule1: function(info) {
     return {
-      url: `/index.html#/m1?id=${info.objId}`
+      url: `/index.html#/m1`
     }
   },
   openModule2: function(info) {
     return {
-      url: `/index.html#/m2?id=${info.objId}`
+      url: `/index.html#/m2`
     }
   },
   openModule3: function(info) {
     return {
-      url: `/index.html#/m3?id=${info.objId}`
+      url: `/index.html#/m3`
+    }
+  },
+  openEnvHistory: function() {
+    return {
+      url: `/index.html#/m3`
     }
   }
 }
--- production/src/modules/Module3.tsx
+++ production/src/modules/Module3.tsx
@@ -2,13 +2,59 @@ import * as React from 'react'
 import { Button } from '@mstr/web-components'
 import '@mstr/web-components/dist/web-styles.css'
 import './Module3.scss'
+// import { environments } from 'workstation'
 
+declare var workstation: any
 export default class Module3 extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props)
+    this.state = { currentEnv: null, envChangeList: [] }
+  }
+  
+  componentWillMount() {
+    workstation.environments.getCurrentEnvironment().then((env:any) => {
+      this.setState({currentEnv: env})
+    })
+    workstation.environments.onEnvironmentChange((change: any) => {
+      console.dir(change)
+      var updateList = this.state.envChangeList
+      updateList.push(change)
+      this.setState({envChangeList: updateList})
+    })
+  }
+  
+  renderEnvDetail(env: any) {
+    return (
+      env ?
+      <ul>
+        {
+          Object.keys(env).map((key: string) => {
+            return (<li>{key}: {env[key]}</li>)
+          })
+        }
+      </ul> : null
+    )
+  }
+
   render() {
     return (
       <div>
-        <Button primary={true} text="Click me!"></Button>
-        <div className="module3-welcome">hello module 3!</div>
+        <div className="module3-welcome">{ this.state.currentEnv ? <h1>Current Environment</h1> : <h1>No Current Environment, try context menu on one selected object.</h1> }</div>
+        <div>
+          {this.renderEnvDetail(this.state.currentEnv)}
+        </div>
+        <div>
+          {
+            this.state.envChangeList ? this.state.envChangeList.map((envChange: any) => {
+              return (<div>
+                <h2>Environment Change Type: {envChange.ChangeType}</h2>
+                {Object.keys(envChange.WorkstationEnvironments).map((key: any) => {
+                  return this.renderEnvDetail(envChange.WorkstationEnvironments[key].EnvironmentDTO)
+                })}
+              </div>)
+            }) : <div>No env change list!</div>
+          }
+        </div>
       </div>
     )
   }
