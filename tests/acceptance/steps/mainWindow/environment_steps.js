2089fa593be91abe776114a2329a8678d37a2f4e
kasulanati,sarada <skasulanati@microstrategy.com>
2019-11-07 11:03:55 -0500

    Revert "Skip adding environment if env is already added"
    This reverts commit ad77eef0a1980aeee0a7a8e14f0b96a879b64cb8.

--- tests/acceptance/steps/mainWindow/environment_steps.js
+++ tests/acceptance/steps/mainWindow/environment_steps.js
@@ -1,84 +1,48 @@
-const { Given, When, Then } = require("cucumber");
 
-When(
-  "I add a new environment named {envName} with url {envUrl}",
-  async function(envName, envUrl) {
-    if (!mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
-      return mainWindow.mainCanvas.envSection.connectEnv(envName, envUrl);
-    } else {
-      console.error(
-        `WARNING: Environment already present with name ${envName} \n Skipping this step as ${envName} is already added.`
-      );
-    }
-  }
-);
+const { Given, When, Then } = require('cucumber');
 
-When(
-  "I login with {loginMode} mode using name {userName} and password {userPwd}",
-  async function(loginMode, userName, userPwd) {
-    if (!mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
-      if (userPwd === "blank") userPwd = "";
-      return mainWindow.mainCanvas.envSection.loginToEnv(
-        loginMode,
-        userName,
-        userPwd
-      );
-    } else {
-      console.error(
-        `WARNING: Environment already present with name ${envName} \n Skipping this step as ${envName} is already added.`
-      );
-    }
-  }
-);
+  When('I add a new environment named {envName} with url {envUrl}', async function (envName, envUrl) {
+    return mainWindow.mainCanvas.envSection.connectEnv(envName, envUrl);
+  });
 
-Then("I select project {projectName}", async function(projectName) {
-  if (!mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
+  When('I login with {loginMode} mode using name {userName} and password {userPwd}', async function (loginMode, userName, userPwd) {
+    if(userPwd==='blank')
+      userPwd = "";
+    return mainWindow.mainCanvas.envSection.loginToEnv(loginMode, userName, userPwd);
+  });
+
+  Then('I select project {projectName}', async function (projectName) {
     return mainWindow.mainCanvas.envSection.chooseProject(projectName);
-  } else {
-    console.error(
-      `WARNING: Environment already present with name ${envName} \n Skipping this step as ${envName} is already added.`
-    );
-  }
-});
+  });
 
-When("I click OK to connect after selecting project", async function() {
-  if (!mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
+  When('I click OK to connect after selecting project', async function () {
     return mainWindow.mainCanvas.envSection.clickOkToConnect();
-  } else {
-    console.error(
-      `WARNING: Environment already present with name ${envName} \n Skipping this step as ${envName} is already added.`
-    );
-  }
-});
+  });
 
-Then("I remove environment {envName}", async function(envName) {
-  if (mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
-    return mainWindow.mainCanvas.envSection.removeEnv(envName);
-  } else {
-    console.error(`WARNING: No environment present with name ${envName}`);
-  }
-});
+  Then('I remove environment {envName}', async function (envName) {
+    if (mainWindow.mainCanvas.envSection.isEnvAdded(envName)) {
+      return mainWindow.mainCanvas.envSection.removeEnv(envName);
+    } else {
+      console.error(`WARNING: No environment present with name ${envName}`);
+    }
+  });
 
-When("I configure LDAP service on env {envName}", async function(envName) {
-  return mainWindow.mainCanvas.envSection.configureLdapService(envName);
-});
+  When('I configure LDAP service on env {envName}', async function(envName){
+    return mainWindow.mainCanvas.envSection.configureLdapService(envName);
+  })
 
-Then("I disconnect environment", async function() {
-  return mainWindow.mainCanvas.envSection.disconnectEnv(
-    browser.params.envInfo[0].envName
-  );
-});
+  Then('I disconnect environment', async function(){
+    return mainWindow.mainCanvas.envSection.disconnectEnv(browser.params.envInfo[0].envName);
+  })
 
-Then("I connect existing environment {name}", async function(name) {
-  return mainWindow.mainCanvas.envSection.connectExistingEnv(name);
-});
+  Then('I connect existing environment {name}', async function(name){
+    return mainWindow.mainCanvas.envSection.connectExistingEnv(name);
+  })
 
-When("I import LDAP users on env {envName}", async function(envName) {
-  return mainWindow.mainCanvas.envSection.importLdapUsers(envName);
-});
+  When('I import LDAP users on env {envName}', async function(envName){
+    return mainWindow.mainCanvas.envSection.importLdapUsers(envName);
+  })
 
-Then("{envName} is connected", async function(envName) {
-  return expect(mainWindow.mainCanvas.envSection.isEnvAdded(envName)).become(
-    true
-  );
-});
+  Then('{envName} is connected', async function (envName) {
+    return expect(mainWindow.mainCanvas.envSection.isEnvAdded(envName)).become(true);
+  });
