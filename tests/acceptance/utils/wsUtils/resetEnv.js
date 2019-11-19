696d0193a9b526b1a93ee9439ffaacf9c9a427b4
kasulanati,sarada <skasulanati@microstrategy.com>
2019-11-19 17:59:06 -0500

    Don't run remove env, if no env exists

--- tests/acceptance/utils/wsUtils/resetEnv.js
+++ tests/acceptance/utils/wsUtils/resetEnv.js
@@ -2,11 +2,16 @@ const execSync = require("child_process").execSync;
 let removeEnv;
 
 function resetEnv() {
-  removeEnv = execSync(`defaults delete ~/Library/Preferences/com.microstrategy.Workstation 4c158b61ebc73998d366d08f49c1bed507a0264f`);
-  console.log(`Resetting Environments: ${removeEnv}`);
-  removeEnv = execSync(`defaults delete ~/Library/Preferences/com.microstrategy.Workstation acca64b83535b9a1dff37a13cd176f393be7c195`);
-  console.log(`Resetting Environments: ${removeEnv}`);
-  execSync(`defaults read ~/Library/Preferences/com.microstrategy.Workstation`);
+  try {
+    removeEnv = execSync(`defaults delete ~/Library/Preferences/com.microstrategy.Workstation 4c158b61ebc73998d366d08f49c1bed507a0264f`);
+    console.log(`Resetting Environments: ${removeEnv}`);
+    removeEnv = execSync(`defaults delete ~/Library/Preferences/com.microstrategy.Workstation acca64b83535b9a1dff37a13cd176f393be7c195`);
+    console.log(`Resetting Environments: ${removeEnv}`);
+    execSync(`defaults read ~/Library/Preferences/com.microstrategy.Workstation`);
+  } catch (error) {
+    console.log("No environments present to remove");
+    console.log(`${error}\n`);
+  }
 }
 
 module.exports = resetEnv;
