d28e0d8339686c368eb73a2c8dc9ace555d580ed
luntan <luntan@microstrategy.com>
2020-01-02 16:00:32 -0500

    extract target file name for cucumber builder

--- tests/acceptance/utils/ubUtils/ubBuilderReplacer.js
+++ tests/acceptance/utils/ubUtils/ubBuilderReplacer.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
 
-const fileName = 'finalizeHelpers.js'
+const sourceFileName = 'finalizeHelpers.js'
+const targetFileName = 'finalize_helpers.js'
 // replace the builder.js in node_modules to the builder.js in utils/ubUtils/ . After the replacement, the step information in cucumber will become available to the UB monitor.
 function replaceUBBuilder() {
-  const cucumberBuilderPath = `./node_modules/cucumber/lib/support_code_library_builder/finalize_helpers.js`
+  const cucumberBuilderPath = `./node_modules/cucumber/lib/support_code_library_builder/${targetFileName}`
 
   try {
     fs.unlinkSync(cucumberBuilderPath)
@@ -12,7 +13,7 @@ function replaceUBBuilder() {
     console.log(err)
   }
 
-  const ubBuilderPath = `./utils/ubUtils/${fileName}`
+  const ubBuilderPath = `./utils/ubUtils/${sourceFileName}`
   var newUBBuilderContent = fs.readFileSync(ubBuilderPath, 'utf8')
 
   console.info(`re-generating ${cucumberBuilderPath}`)
