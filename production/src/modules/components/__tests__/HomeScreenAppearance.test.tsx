ea072152a5502b412bbd5d21b52d4b5f17c47d84
Tejas Sahasranaman <tsahasranaman@microstrategy.com>
2022-04-21 18:18:19 -0400

    Fix ut

--- production/src/modules/components/__tests__/HomeScreenAppearance.test.tsx
+++ production/src/modules/components/__tests__/HomeScreenAppearance.test.tsx
@@ -72,8 +72,8 @@ describe('HomeScreenAppearance Component', () => {
   it('No Theme Test', async () => {
     const newRootState = { ...rootState };
     newRootState.configEditor.currentConfig.homeScreen.theme = undefined
-    
-    const store = createStore(rootState);
+
+    const store = createStore(newRootState);
     // Render
     const { queryByLabelText, queryAllByRole } = render(
       <Provider store={store}>
