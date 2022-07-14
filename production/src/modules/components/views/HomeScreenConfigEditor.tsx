ce41226ffbc69d30ba3f609f3004e341474210d2
haoxu <haoxu@microstrategy.com>
2022-07-14 10:55:11 +0800

    US412106; revert feature flag debug

--- production/src/modules/components/views/HomeScreenConfigEditor.tsx
+++ production/src/modules/components/views/HomeScreenConfigEditor.tsx
@@ -615,7 +615,7 @@ class HomeScreenConfigEditor extends React.Component<any, any> {
                                     </Tabs.TabPane>
                                 )}
 
-                                {true && (
+                                {this.state.customEmailFeatureEnabled && (
                                 <Tabs.TabPane
                                     tab={localizedStrings.NAVBAR_CUSTOM_EMAIL_SETTINGS}
                                     key={VC.CUSTOMEMAILSETTINGS}
