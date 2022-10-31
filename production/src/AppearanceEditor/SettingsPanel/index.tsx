3ce6a4234298f99e0d84c12adda7f9f1b9bd1212
Tejas Sahasranaman <tsahasranaman@microstrategy.com>
2022-10-31 11:08:09 -0400

    remove unnecessary react frangment

--- production/src/AppearanceEditor/SettingsPanel/index.tsx
+++ production/src/AppearanceEditor/SettingsPanel/index.tsx
@@ -6,15 +6,13 @@ import './styles.scss';
 
 const SettingsPanel: React.FC = () => {
     return (
-        <React.Fragment>
-            <div className="mstr-app-theme-settings-panel">
-                <div className="settings-panel-content">
-                    <Logos />
-                    <div className="mstr-divider"></div>
-                    <Color />
-                </div>
+        <div className="mstr-app-theme-settings-panel">
+            <div className="settings-panel-content">
+                <Logos />
+                <div className="mstr-divider"></div>
+                <Color />
             </div>
-        </React.Fragment>
+        </div>
     );
 };
 
