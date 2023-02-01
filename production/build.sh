073b414cb1a7015da3da8b0077c7e1bec5b629c3
haoxu <haoxu@microstrategy.com>
2023-02-01 13:16:21 +0800

    change build to test log

--- production/build.sh
+++ production/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 cd /mnt/production
-export NODE_OPTIONS="--max-old-space-size=2048"
+export NODE_OPTIONS="--max-old-space-size=4096"
 yarn install --fronzen-lockfile
+ls -l
 yarn buildWS
 yarn updateVersion --env.APPLICATION_VERSION $APPLICATION_VERSION
