1d702dfebabb39e09ddbd58449b0d928a5adfa68
haoxu <haoxu@microstrategy.com>
2023-02-01 15:10:03 +0800

    test the lock-file

--- production/build.sh
+++ production/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /mnt/production
 export NODE_OPTIONS="--max-old-space-size=4096"
-yarn install --fronzen-lockfile
+ls -l
+yarn install
 ls -l
 yarn buildWS
 yarn updateVersion --env.APPLICATION_VERSION $APPLICATION_VERSION
