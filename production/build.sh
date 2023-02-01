89594a4f109dece86c10b67e9e79bbb3796bf515
haoxu <haoxu@microstrategy.com>
2023-02-01 15:56:44 +0800

    remove install

--- production/build.sh
+++ production/build.sh
@@ -2,8 +2,8 @@
 echo $(whoami)
 cd /mnt/production
 export NODE_OPTIONS="--max-old-space-size=4096"
-ls -l
-yarn install
+# ls -l
+# yarn install
 ls -l
 yarn buildWS
 yarn updateVersion --env.APPLICATION_VERSION $APPLICATION_VERSION
