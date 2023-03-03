80597b4547ab1c63bbb75e747df8c3726c5f84ef
Ma, Yifu <yima@microstrategy.com>
2023-03-03 15:28:03 -0500

    Update nodejs cookbook version to 16.19.1

--- workspace-settings/shell/set.env.bash
+++ workspace-settings/shell/set.env.bash
@@ -33,7 +33,7 @@ else
   export WS_TEST_PLATFORM=mac
 fi
 
-nodejs_version="16.19.0"
+nodejs_version="16.19.1"
 export nodejs_image_tag=$nodejs_version
 if uname -a | grep -q "Darwin"; then
   export NODE_HOME=/usr/local/nodejs-binary-$nodejs_version
