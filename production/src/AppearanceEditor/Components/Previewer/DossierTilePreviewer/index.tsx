d657b2af4f965f9c989b1e32e5c83a10110e40b1
Vargas <jvargas@TYCM15978.local>
2023-04-27 14:52:33 -0600

    add Dossier Name string to Previewer

--- production/src/AppearanceEditor/Components/Previewer/DossierTilePreviewer/index.tsx
+++ production/src/AppearanceEditor/Components/Previewer/DossierTilePreviewer/index.tsx
@@ -13,9 +13,7 @@ type DossierTilePreviewerType = {
 const DossierTilePreviewer: React.FC<DossierTilePreviewerType> = ({ deviceType, isNoTheme, isDarkTheme, previewerClassName }) => {
     return <div className={previewerClassName(deviceType, '-library-dossier-tile')}>
         <div className='dossier-background' />
-        <div className={classnames('dossier-name', { 'no-theme': isNoTheme }, { 'dark-theme': isDarkTheme })}>
-            Dossier Name
-        </div>
+        <div className={classnames('dossier-name', { 'no-theme': isNoTheme }, { 'dark-theme': isDarkTheme })}>{localizedStrings.DOSSIER_NAME}</div>
     </div>
 };
 
