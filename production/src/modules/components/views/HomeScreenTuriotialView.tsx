899cd8d4703ced71dbc870f17b4820cdbcf08f7c
kai <kai@kais-Mac-mini.lan>
2023-06-07 01:02:54 +0800

    add page

--- production/src/modules/components/views/HomeScreenTuriotialView.tsx
+++ production/src/modules/components/views/HomeScreenTuriotialView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-import { Timeline, Layout } from 'antd';
+import { Layout, Carousel } from 'antd';
 import { TAGS } from '../HomeScreenConfigConstant';
 import { selectFeatures } from 'src/store/selectors/HomeScreenConfigEditorSelector';
 import { RootState } from 'src/types/redux-state/HomeScreenConfigState';
@@ -8,9 +8,26 @@ import * as Actions from '../../../store/actions/ActionsCreator';
 
 let availableTags: Array<TAGS> = [TAGS.action, TAGS.info, TAGS.share];
 
+type AppearanceEditorProps = {
+  features: Array<TAGS>
+}
+
 class HomeScreenTuriotialView extends React.Component<any, any> {
     render(): React.ReactNode {
-        return <HomeScreenTuriotialViewComponent></HomeScreenTuriotialViewComponent>
+      // const [tags, setTags] = React.useState(availableTags);
+      const { features } = this.props;
+      const onChange = (currentSlide: number) => {
+        console.log(currentSlide);
+      };
+      const items = features.features.map(
+        (item: TAGS) => <HomeScreenTuriotialViewComponent item={item}/>
+      )
+
+      return (
+        <Carousel afterChange={onChange}>
+            {items}
+        </Carousel>
+      );
     }
 }
 
@@ -22,16 +39,43 @@ const connector = connect(mapState, {
   setfeatures: Actions.setFeatures
 })
 
-const HomeScreenTuriotialViewComponent: React.FC<any> = (any) => {
-    const [tags, setTags] = React.useState(availableTags);
-    const items = tags.map(
-      (item) => <Timeline.Item>{item}</Timeline.Item>
-    );
-    return (
-      <Timeline >
-        {items}
-      </Timeline>
-    );
+
+
+const overStyle: React.CSSProperties = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  textAlign: 'center',
+  // background: '#364d79',
+};
+
+const headerStyle: React.CSSProperties = {
+  height: '30px',
+  color: '#f00'
+};
+
+const contentStyle: React.CSSProperties = {
+  marginTop: '30px',
+  height: '100px',
+};
+
+// 1 question and 2 options
+const HomeScreenTuriotialViewComponent: React.FC<any> = (props: any) => {
+  const onChange = (item) => {
+    console.log(item)
+  }
+
+  return <div>
+    <h3 style={overStyle}>
+      <Layout>
+        <Layout.Header style={headerStyle}>{props.item}</Layout.Header>
+        <Layout.Content style={contentStyle}>
+          <button onClick={onChange}>YES</button>
+          <button onClick={onChange}>NO</button>
+        </Layout.Content>
+      </Layout>
+    </h3>
+  </div>
 }
 
 export default connector(HomeScreenTuriotialView);
